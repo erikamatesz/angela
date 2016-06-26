594a7ccae8166383b3078ccb968e2308efae8981
mcseves <mcmseves@gmail.com>
2016-06-26 02:51:56 -0300

    notification

--- angela/angela/AppDelegate.swift
+++ angela/angela/AppDelegate.swift
@@ -10,10 +10,10 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
-
-
+    
+    
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         window = UIWindow(frame: UIScreen.mainScreen().bounds)
         
@@ -22,14 +22,36 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window?.rootViewController = mainViewController
         window?.makeKeyAndVisible()
         
-        
         //Habilitando notificações
         let notificationTypes: UIUserNotificationType = [UIUserNotificationType.Alert, UIUserNotificationType.Badge, UIUserNotificationType.Sound]
         let pushNotificationSettings = UIUserNotificationSettings(forTypes: notificationTypes, categories: nil)
         application.registerUserNotificationSettings(pushNotificationSettings)
         application.registerForRemoteNotifications()
         
-
+        
+        let textAction = UIMutableUserNotificationAction()
+        textAction.identifier = "TEXT_ACTION"
+        textAction.title = "Reply"
+        textAction.activationMode = .Background
+        textAction.authenticationRequired = false
+        textAction.destructive = false
+        textAction.behavior = .TextInput
+        
+        enum UIUserNotificationActionBehavior : UInt {
+            case Default // the default action behavior
+            case TextInput // system provided action behavior, allows text input from the user
+        }
+        
+        let category = UIMutableUserNotificationCategory()
+        category.identifier = "CATEGORY_ID"
+        category.setActions([textAction], forContext: .Default)
+        category.setActions([textAction], forContext: .Minimal)
+        
+        let categories = NSSet(object: category) as! Set<UIUserNotificationCategory>
+        let settings = UIUserNotificationSettings(forTypes: [.Alert, .Badge, .Sound], categories: categories)
+        UIApplication.sharedApplication().registerUserNotificationSettings(settings)
+        
+        
         return true
         
     }
@@ -43,29 +65,29 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: NSError) {
         print(error)
     }
-
+    
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
     }
-
+    
     func applicationDidEnterBackground(application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
-
+    
     func applicationWillEnterForeground(application: UIApplication) {
         // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
     }
-
+    
     func applicationDidBecomeActive(application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
-
+    
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
-
+    
+    
 }
 
